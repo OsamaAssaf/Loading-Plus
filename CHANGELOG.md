106b89f0bb2e69dbe1aef66544b1e241a1b9f488
OsamaAssaf <osama.assaf.o@gmail.com>
2023-07-24 18:58:08 +0300

    version 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,12 +2,17 @@
 
 All notable changes to the "Loading Plus" package will be documented in this file.
 
-## [1.1.0] - 2023-07-24
+## 1.2.0
+
+### Added
+- Added comments to improve code readability and understanding.
+
+## 1.1.0
 
 ### Added
 - Added an example application demonstrating how to use the "Loading Plus" package.
 
-## [1.0.0] - 2023-07-23
+## 1.0.0
 
 ### Added
 - Initial release of the "Loading Plus" package.
--- lib/loading_plus.dart
+++ lib/loading_plus.dart
@@ -1,42 +1,55 @@
+// Importing required libraries
 library loading_plus;
 
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+// Defining the LoadingPlus class
 class LoadingPlus {
+  // Private constructor to prevent external instantiation
   LoadingPlus._();
 
+  // Singleton instance of the LoadingPlus class
   static final LoadingPlus _instance = LoadingPlus._();
 
+  // Getter to access the singleton instance
   static LoadingPlus get instance => _instance;
 
+  // Global key to keep track of the navigator state
   GlobalKey<NavigatorState>? _key;
 
+  // Initialization method to set the navigator key
   void init(GlobalKey<NavigatorState> navigatorKey) {
     _key = navigatorKey;
   }
 
+  // Method to show a default loading dialog with a CircularProgressIndicator
   void show({
     Color? color,
     bool barrierDismissible = false,
     bool willPopScope = false,
     ImageFilter? backdropFilter,
   }) {
+    // Check if the navigator key is set
     if (_key == null) {
       throw Exception(
           'You must use LoadingPlus.loadingInit(navigatorKey) before calling LoadingPlus.show()');
     }
+    // Set a default backdrop filter with zero blur
     backdropFilter = ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0);
+    // Show the loading dialog
     showDialog(
       context: _key!.currentContext!,
       barrierDismissible: barrierDismissible,
       builder: (_) {
+        // Wrap the dialog in a WillPopScope to handle back button presses
         return WillPopScope(
           onWillPop: () async => willPopScope,
           child: BackdropFilter(
             filter: backdropFilter!,
             child: Center(
+              // Show a CircularProgressIndicator in the center of the dialog
               child: CircularProgressIndicator(
                 color: color,
               ),
@@ -47,6 +60,7 @@ class LoadingPlus {
     );
   }
 
+  // Method to show a loading dialog with an asset image in the center
   void showAssetImage({
     required String image,
     double width = 24.0,
@@ -55,20 +69,25 @@ class LoadingPlus {
     bool willPopScope = false,
     ImageFilter? backdropFilter,
   }) {
+    // Check if the navigator key is set
     if (_key == null) {
       throw Exception(
           'You must use LoadingPlus.loadingInit(navigatorKey) before calling LoadingPlus.showAssetImage()');
     }
+    // Set a default backdrop filter with zero blur
     backdropFilter = ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0);
+    // Show the loading dialog
     showDialog(
       context: _key!.currentContext!,
       barrierDismissible: barrierDismissible,
       builder: (_) {
+        // Wrap the dialog in a WillPopScope to handle back button presses
         return WillPopScope(
           onWillPop: () async => willPopScope,
           child: BackdropFilter(
             filter: backdropFilter!,
             child: Center(
+              // Show an Image.asset widget with the specified asset image in the center
               child: Image.asset(
                 image,
                 width: width,
@@ -81,6 +100,7 @@ class LoadingPlus {
     );
   }
 
+  // Method to show a loading dialog with a network image in the center
   void showNetworkImage({
     required String image,
     double width = 24.0,
@@ -89,20 +109,25 @@ class LoadingPlus {
     bool willPopScope = false,
     ImageFilter? backdropFilter,
   }) {
+    // Check if the navigator key is set
     if (_key == null) {
       throw Exception(
           'You must use LoadingPlus.loadingInit(navigatorKey) before calling LoadingPlus.showNetworkImage()');
     }
+    // Set a default backdrop filter with zero blur
     backdropFilter = ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0);
+    // Show the loading dialog
     showDialog(
       context: _key!.currentContext!,
       barrierDismissible: barrierDismissible,
       builder: (_) {
+        // Wrap the dialog in a WillPopScope to handle back button presses
         return WillPopScope(
           onWillPop: () async => willPopScope,
           child: BackdropFilter(
             filter: backdropFilter!,
             child: Center(
+              // Show an Image.network widget with the specified network image in the center
               child: Image.network(
                 image,
                 width: width,
@@ -115,19 +140,23 @@ class LoadingPlus {
     );
   }
 
+  // Method to show a custom loading dialog with a custom child widget
   void showCustom({
     required Widget child,
     bool barrierDismissible = false,
     bool willPopScope = false,
   }) {
+    // Check if the navigator key is set
     if (_key == null) {
       throw Exception(
           'You must use LoadingPlus.loadingInit(navigatorKey) before calling LoadingPlus.showCustom()');
     }
+    // Show the loading dialog
     showDialog(
       context: _key!.currentContext!,
       barrierDismissible: barrierDismissible,
       builder: (_) {
+        // Wrap the dialog in a WillPopScope to handle back button presses
         return WillPopScope(
           onWillPop: () async => willPopScope,
           child: child,
@@ -136,14 +165,18 @@ class LoadingPlus {
     );
   }
 
+  // Method to dismiss the currently displayed loading dialog
   void dismiss() {
+    // Check if the navigator key is set
     if (_key == null) {
       throw Exception(
           'You must use LoadingPlus.loadingInit(navigatorKey) before calling LoadingPlus.dismiss()');
     }
+    // Dismiss the loading dialog
     Navigator.of(_key!.currentContext!).pop();
   }
 
+  // Utility method to create a default loading widget with a CircularProgressIndicator
   Center loadingWidget({
     Color? color,
   }) {
@@ -154,6 +187,7 @@ class LoadingPlus {
     );
   }
 
+  // Utility method to create a custom loading widget with a provided child widget
   Center loadingWidgetCustom({
     required Widget child,
   }) {
